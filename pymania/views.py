eaed6ec4bd4616c42749a37ed090e7cf9229a3d6
Tanmay Vaij <tanmayvaij22@gmail.com>
2024-12-01 12:19:46 +0530

    added reqstxt
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

--- pymania/views.py
+++ pymania/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 
+
 def home(request):
+
+    if request.method == "POST":
+
+        data = {
+            "name": request.POST.get("name"),
+            "email": request.POST.get("email"),
+            "phonenumber": request.POST.get("phonenumber")
+        }
+
+        return render(request, "index.html", data)
     return render(request, "index.html")
